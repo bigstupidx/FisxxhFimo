04e2e41a37742dc34bda34badfd6f3fb97be5061
tobi994 <viettt.fuky@gmail.com>
2016-05-28 23:27:19 +0700

    +some particle water

---
+++ Fishing/Assets/JMO Assets/Cartoon FX/Scripts/CFX_AutoDestructShuriken.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class CFX_AutoDestructShuriken : MonoBehaviour
+{
+	public bool OnlyDeactivate;
+
+    public float minPlay = 1.0f;
+    public float maxPlay = 3.0f;
+	
+    void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+	void OnEnable()
+	{
+		StartCoroutine("CheckIfAlive");
+        StartEffect();
+	}
+	
+	IEnumerator CheckIfAlive ()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(0.5f);
+			if(!GetComponent<ParticleSystem>().IsAlive(true))
+			{
+				if(OnlyDeactivate)
+				{
+					#if UNITY_3_5
+						this.gameObject.SetActiveRecursively(false);
+					#else
+						this.gameObject.SetActive(false);
+					#endif
+				}
+				else
+					//GameObject.Destroy(this.gameObject);
+				break;
+			}
+		}
+	}
+
+    private ParticleSystem particle;
+
+    public void StartEffect()
+    {
+        StartCoroutine(Effect());
+    }
+
+    private IEnumerator Effect()
+    {
+        while(true)
+        {
+            if(!particle.isPlaying)
+            {
+                particle.Play();
+            }
+            yield return new WaitForSeconds(Random.Range(minPlay, maxPlay));
+        }
+    }
+}
---
+++ Fishing/Assets/Script/CScreenManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum eScreenType
+{
+    MENU = 1,
+    GAME_PLAY =2,
+    GAME_FINISH = 3
+}
+
+[System.Serializable]
+public class ScreenConfig
+{
+    public GameObject _objScreen;
+    public eScreenType _type;
+}
+public class CScreenManager : MonoBehaviour {
+    public List<ScreenConfig> listScreen = new List<ScreenConfig>();
+    private Dictionary<eScreenType, GameObject> dicScreen = new Dictionary<eScreenType, GameObject>();
+
+    public GameObject currScreen;
+    void Awake()
+    {
+        InitDictionary();
+    }
+	// Use this for initialization
+	void Start () {
+        ShowScreenByType(eScreenType.MENU);
+	}
+    public void InitDictionary()
+    {
+        foreach(ScreenConfig var in listScreen)
+        {
+            dicScreen.Add(var._type, var._objScreen);
+        }
+    }
+	
+    private GameObject GetScreenByType(eScreenType type)
+    {
+        if(dicScreen.ContainsKey(type))
+        {
+            return dicScreen[type];
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log("ko co loai man hinh nay");
+#endif
+            return null;
+        }
+    }
+
+    public void ShowScreenByType(eScreenType type)
+    {
+        DisableAllScreen();
+        currScreen = GetScreenByType(type);
+        if(currScreen)
+        {
+            currScreen.SetActive(true);
+        }
+    }
+
+    private void DisableAllScreen()
+    {
+        foreach(ScreenConfig var in listScreen)
+        {
+            var._objScreen.SetActive(false);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
